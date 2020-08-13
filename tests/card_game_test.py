bcfbebda6eb906d0301da062c12c18fa5fbb62a1
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-13 14:32:58 +0100

    Adds tests for checking if the card is an ace true or false

--- tests/card_game_test.py
+++ tests/card_game_test.py
@@ -6,7 +6,13 @@ from src.card_game import CardGame
 class CardGameTest(unittest.TestCase):
     def setUp(self):
         self.card = Card("Spades", 1)
+        self.card_2 = Card("Hearts", 2)
+        self.game = CardGame()
     
-    def test_check_for_ace(self):
-        actual = CardGame.checkforAce(self.card)
+    def test_check_for_ace_true(self):
+        actual = self.game.checkforAce(self.card)
         self.assertTrue(actual)
+
+    def test_check_for_ace_false(self):
+        actual = self.game.checkforAce(self.card_2)
+        self.assertFalse(actual)
